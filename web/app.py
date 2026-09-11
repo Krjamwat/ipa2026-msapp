9c2e1995349e864455405f25fa60efc7ce130e15
Krittapat <67070001@kmitl.ac.th>
2026-09-11 16:53:10 +0700

    reformatted with black

--- web/app.py
+++ web/app.py
@@ -28,11 +28,7 @@ def add_router():
     password = request.form.get("password")
 
     if ip and username and password:
-        routers.insert_one({
-            "ip": ip,
-            "username": username,
-            "password": password
-        })
+        routers.insert_one({"ip": ip, "username": username, "password": password})
     return redirect(url_for("main"))
 
 
@@ -47,8 +43,9 @@ def delete_router(id):
 
 @app.route("/router/<ip>")
 def router_detail(ip):
-    records = list(interface_status.find({"router_ip": ip})
-                   .sort("timestamp", -1).limit(3))
+    records = list(
+        interface_status.find({"router_ip": ip}).sort("timestamp", -1).limit(3)
+    )
     return render_template("router_detail.html", ip=ip, records=records)
 
 
