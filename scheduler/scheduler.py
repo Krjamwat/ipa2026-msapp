689b2f33ca01e6c1e6a67cc692dac59ab352c44f
Krittapat <67070001@kmitl.ac.th>
2026-09-11 22:52:18 +0700

    do exercise

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -6,7 +6,7 @@ from producer import produce
 
 
 def scheduler():
-    INTERVAL = 10.0
+    INTERVAL = 30.0
     next_run = time.monotonic()
     count = 0
     rabbitmq_host = os.environ.get("RABBITMQ_HOST", "rabbitmq")
